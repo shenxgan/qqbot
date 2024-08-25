3e6c6f95e5f4f87890be697478f90fb9a7410b39
shengan <shengan@popmart.com>
2024-08-25 15:04:38 +0800

    添加频率限制

--- ws/plugins/welcome/main.py
+++ ws/plugins/welcome/main.py
@@ -1,3 +1,5 @@
+import time
+
 from plugins.base import Base
 
 
@@ -6,11 +8,17 @@ class Plugin(Base):
     def __init__(self):
         super().__init__()
         self.type = 'notice'
+        self.last_ts = {}       # 记录每个群的触发时间
+        self.cd = 120           # 同一群回复间隔为2分钟
 
     def is_match(self, data):
         """检测是否匹配此插件"""
         if data.get('notice_type') == 'group_increase':
-            return True
+            group_id = data['group_id']
+            now = time.time()
+            if now - self.last_ts.get(group_id, 0) > self.cd:
+                self.last_ts[group_id] = now
+                return True
         else:
             return False
 
