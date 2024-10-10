6f96fee9865c6aa60462265760a1c2983845095a
shengan <shengan@popmart.com>
2024-10-10 17:01:34 +0800

    群专属头衔自助设置

--- ws/plugins/admin/main.py
+++ ws/plugins/admin/main.py
@@ -14,33 +14,48 @@ class Plugin(Base):
         self.ws = None
         self.data = None
         self.ats = None
-
-    def is_match(self, message):
-        """检测是否匹配此插件"""
-        if self.data['sender']['role'] not in {'owner', 'admin'}:
-            return False
-        return True
+        self.done = set()
+        self.is_tx_self = False
 
     async def set_group_special_title(self, message):
         """设置专属头衔"""
+        if self.data['sender']['role'] not in {'owner', 'admin'}:
+            return None
         if not self.ats:
             return None
-        tx = message.strip()[:6]
+        tx = message.strip()
         for at in self.ats:
             re_s = r'qq=(\d+),*'
             info = re.findall(re_s, at)
-            qq = info[0]
-            ret = {
-                'action': 'set_group_special_title',
-                'params': {
-                    'group_id': self.data['group_id'],
-                    'user_id': int(qq),
-                    'special_title': tx,
-                }
-            }
-            await self.ws.send(json.dumps(ret))
+            qq = int(info[0])
+            await self.set_special_title(qq, tx)
         msg = '专属头衔设置成功'
         return msg
+
+    async def set_group_special_title_self(self, message):
+        """设置专属头衔-自助"""
+        if self.is_tx_self is False:
+            return None
+        qq = self.data['sender']['user_id']
+        if qq in self.done:
+            return None
+        # self.done.add(qq)
+        tx = message.strip()
+        await self.set_special_title(qq, tx)
+        msg = '专属头衔设置成功'
+        return msg
+
+    async def set_special_title(self, qq, tx):
+        """设置专属头衔"""
+        ret = {
+            'action': 'set_group_special_title',
+            'params': {
+                'group_id': self.data['group_id'],
+                'user_id': qq,
+                'special_title': tx,
+            }
+        }
+        await self.ws.send(json.dumps(ret))
 
     def web_sign(self, message):
         """获取或设置 web qq 访问的 sign 值"""
@@ -64,8 +79,15 @@ class Plugin(Base):
             msg = await self.set_group_special_title(message)
         elif message[:2] == '头衔':
             message = message[2:]
-            msg = await self.set_group_special_title(message)
+            msg = await self.set_group_special_title_self(message)
         elif message[:5] == r'\sign':
             message = message[5:]
             msg = self.web_sign(message)
+        elif '专属头衔' in message:
+            if self.is_tx_self is True:
+                return
+            if self.data['user_id'] != self.data['self_id']:
+                return None
+            self.is_tx_self = True
+            msg = '【专属头衔-自助设置】启动！\n示例：头衔 天才萌新'
         return msg
